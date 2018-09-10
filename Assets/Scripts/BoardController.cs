1fd02cf1f3780cc0991d3b672844e6756fcdd454
Fl0ps <florian.etgeton@gmail.com>
2018-09-10 12:57:46 +0200

    Shoe and stuff

---
+++ Assets/Scripts/BoardController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardController : MonoBehaviour {
+
+    private SteamVR_TrackedController controller;
+    private SteamVR_Controller.Device device;
+
+    // Use this for initialization
+    void Start () {
+        controller = this.GetComponent<SteamVR_TrackedController>();
+        device  = SteamVR_Controller.Input((int)controller.controllerIndex);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log("Velocity X: " + device.velocity.x + " --- Velocity Y: " + device.velocity.y + " --- Velocity Z: " + device.velocity.z);
+        // lenken mit Z achse zwischen -10 & 10
+        // bremsen wenn X achse grösser als -30
+        Debug.Log(this.transform.eulerAngles.z);
+	}
+}
