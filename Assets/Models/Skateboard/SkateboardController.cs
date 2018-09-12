fde0cb88a6f80b5a02af2893ad945d723cf2d325
Fl0ps <florian.etgeton@gmail.com>
2018-09-12 16:30:14 +0200

    track, sound, vignette

---
+++ Assets/Models/Skateboard/SkateboardController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkateboardController : MonoBehaviour {
+
+    public PlayerMovement pm;
+
+    private bool braked = false;
+    public GameObject frontTruck;
+    public GameObject backTruck;
+    public GameObject board;
+    public float maxSteeringAngle = 15.0f;
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float translation = Input.GetAxis("Vertical");
+        float rotation = Input.GetAxis("Horizontal");
+
+        //frontTruck.transform.eulerAngles = new Vector3(0, -rotation * maxSteeringAngle, 0);
+        //backTruck.transform.eulerAngles = new Vector3(0, rotation * maxSteeringAngle, 0);
+        //board.transform.eulerAngles = new Vector3(0, 0, rotation * maxSteeringAngle);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Border"))
+        {
+            if (!braked)
+            {
+                Debug.Log("touched the border");
+                pm.Stop();
+                braked = true;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Border"))
+        {
+            braked = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public GameObject controller;
-	private SteamVR_TrackedController controllerScript;
+	public AudioSource skateSound;
+
+    private SteamVR_TrackedController controllerScript;
 	private SteamVR_Controller.Device device;
 
 	private float speed;
 	private float neigung;
+    private float lastNeigung;
 	private float breakSpeed;
 
 
@@ -19,12 +22,17 @@ public class PlayerMovement : MonoBehaviour {
 	void Start () {
 		controllerScript = controller.GetComponent<SteamVR_TrackedController>();
 		device  = SteamVR_Controller.Input((int)controllerScript.controllerIndex);
+        skateSound.loop = true;
 		speed = 0.0f;
-		breakSpeed = 0.005f;
+		breakSpeed = 0.002f;
+
+        //neigung = lastNeigung = 0f;
 	}
 
 	// Update is called once per frame
 	void Update () {
+
+        //Debug.Log("is playing sound: " + skateSound.isPlaying);
 		if(Input.GetKeyDown(KeyCode.Space)){
 			speed += 1;
 		}
@@ -33,29 +41,56 @@ public class PlayerMovement : MonoBehaviour {
 		if (neigung > 180) {
 			neigung = neigung - 360;
 		}
-			
-		if (controller.transform.eulerAngles.x <= 330) {
-			breakSpeed = 0.1f;
+
+        //Debug.Log("actual neigung is: " + neigung);
+        neigung = (lastNeigung + neigung) / 2;
+        //Debug.Log("damped neigung is: " + neigung);
+        lastNeigung = neigung/5;
+
+        if (controller.transform.eulerAngles.x <= 330) {
+			breakSpeed = breakSpeed * 10f;
 		} else {
-			breakSpeed = 0.005f;
+			breakSpeed = breakSpeed / 10f;
 		}
 
 		if (speed > 0) {
-			//Get forward vector of skateboard and set y direction to 0
-			Vector3 fwd = controller.transform.forward;
+
+            if (!skateSound.isPlaying)
+            {
+                skateSound.Play(0);
+                skateSound.time = 0f;
+                //Debug.Log("started playing");
+            }
+
+            //Get forward vector of skateboard and set y direction to 0
+            Vector3 fwd = controller.transform.forward;
 			fwd.y = 0;
 
 			//set the new position times the speed and a damper
 			transform.position += fwd * speed * 0.01f;
-			//rotate the camera rig according to the neigung
+            //rotate the camera rig according to the neigung
+
 			transform.RotateAround (this.transform.position, Vector3.up, -neigung * 0.1f);
 
 			//reduce the speed each frame
 			speed -= breakSpeed;
+		} else {
+            if (skateSound.isPlaying)
+            {
+                //skateSound.Pause ();
+                skateSound.Stop();
+                //skateSound.time = 0;
+                //Debug.Log("stopped playing...");
+            }
 		}
 	}
 
 	public void AddSpeed(float speed){
 		this.speed += speed;
 	}
+
+    public void Stop()
+    {
+        this.speed = 0f;
+    }
 }
